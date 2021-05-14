70577d576de7b54861b942106e752d2845070687
WissaleKH <75116994+WissaleKH@users.noreply.github.com>
2021-05-14 10:02:59 +0200

    Add files via upload

---
+++ mr.py
@@ -0,0 +1,32 @@
+import flask
+from flask import Flask, render_template, request
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+app = Flask(__name__)
+mod = "C:/Users/pc/Desktop/LR2.pkl"
+model = pickle.load(open(mod, 'rb'))
+
+
+@app.route("/")
+def main():
+    return render_template("form.html")
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    data1 = request.form['Age']
+    data2 = request.form['Gender']
+    data3 = request.form['family_history']
+    data4 = request.form['benefits']
+    data5 = request.form['care_options']
+    data6 = request.form['anonymity']
+    data7 = request.form['leave']
+    data8 = request.form['work_interfere']
+    arr = np.array([[data1,data2,data3,data4,data5,data6,data7,data8]])
+    pred = model.predict(arr)
+    return render_template('result.html',result=pred)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
